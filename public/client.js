a55c9e79c6a63dafc3d038efe342dda1ed1e2645
tsumarios <mraciti96@gmail.com>
2025-12-11 19:43:21 +0100

    Update rollback

--- public/client.js
+++ public/client.js
@@ -213,7 +213,7 @@ function handleReaction(data) {
 // --- EVENT BINDINGS ---
 sendBtn.addEventListener('click', sendMessage);
 input.addEventListener('keydown', e => { if (e.key === 'Enter') sendMessage(); });
-nudgeBtn.addEventListener('click', () => { socket.emit('nudge'); nudgeBtn.blur() });
+nudgeBtn.addEventListener('click', () => socket.emit('nudge'));
 input.addEventListener('input', () => {
     socket.emit('typing', {});
     clearTimeout(typingTimer);
